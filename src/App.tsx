d817b7ffb3820777fe414afac68d370cc0340c6b
JoaquimLP <joaquimlp2013@gmail.com>
2026-01-31 14:03:23 -0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,23 @@
-import Button from "./components/button";
-import ButtonIcon from "./components/button-icon";
-import ChevronLeftIcon from "./assets/icons/chevron-left.svg?react";
-import ChevronRightIcon from "./assets/icons/chevron-right.svg?react";
-import Badge from "./components/badge";
-import Alert from "./components/alert";
-import Divider from "./components/divider";
-import InputText from "./components/input-text";
-import SearchIcon from "./assets/icons/search.svg?react";
-import InputCheckbox from "./components/input-checkbox";
-import { InputSingleFile } from "./components/input-single-file";
 
-
+import { BrowserRouter, Routes, Route } from "react-router";
+import PageComponents from "./pages/page-components";
+import LayoutMain from "./pages/layout-main";
+import PageHome from "./pages/page-home";
+import PagePhotoDetails from "./pages/page-photo-details";
+import { useForm } from "react-hook-form";
 export default function App() {
+	const form = useForm();
+	const file = form.watch("file");
+	const fileSrc = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
 	return (
-		<div className="grid gap-7 p-6">
-			<div className="flex gap-3">
-				<Button>Button</Button>
-				<Button variant="secondary">Button</Button>
-				<Button disabled>Button</Button>
-				<Button handling>Loading</Button>
-				<Button icon={ChevronRightIcon}>Próxima Imagem</Button>
-				<Button variant="ghost" size="sm">
-					Button
-				</Button>
-				<Button variant="primary" size="sm">
-					Button
-				</Button>
-			</div>
-
-			<div className="flex gap-3">
-				<ButtonIcon icon={ChevronLeftIcon} />
-				<ButtonIcon icon={ChevronRightIcon} variant="secondary" />
-			</div>
-
-			<div className="flex gap-3">
-				<Badge>Todos</Badge>
-				<Badge>Natureza</Badge>
-				<Badge>Viagem</Badge>
-				<Badge loading>Viagem</Badge>
-				<Badge loading>Viagem</Badge>
-				<Badge loading>Viagem</Badge>
-			</div>
-
-			<div>
-				<Alert>
-					Tamanho máximo: 50MB
-					<br />
-					Você pode selecionar arquivos em PNG, JPG, JPEG ou WEBP
-				</Alert>
-			</div>
-
-			<div>
-				<Divider />
-			</div>
-			<div>
-        <InputText placeholder="Buscar foto" icon={SearchIcon} />
-		<InputCheckbox />
-		<InputSingleFile />
-      </div>
-		</div>
+		<BrowserRouter>
+      <Routes>
+        <Route element={<LayoutMain />}>
+          <Route index element={<PageHome />} />
+          <Route path="/fotos/:id" element={<PagePhotoDetails />} />
+          <Route path="/componentes" element={<PageComponents />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
 	);
 }
---
+++ src/pages/page-home.tsx
@@ -0,0 +1,10 @@
+
+import Text from "../components/text";
+
+export default function PageHome() {
+  return (
+    <>
+      <Text variant="heading-medium">Página inicial</Text>
+    </>
+  );
+}
