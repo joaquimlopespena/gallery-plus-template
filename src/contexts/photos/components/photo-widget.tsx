5fd0fb7cdaedc898892873ac25b221254b9e3c6d
JoaquimLP <joaquimlp2013@gmail.com>
2026-01-31 16:47:05 -0300

    New: atualização e criação de components

---
+++ src/contexts/photos/components/photo-widget.tsx
@@ -0,0 +1,78 @@
+import Badge from "../../../components/badge";
+import { buttonTextVariants, buttonVariants } from "../../../components/button";
+import ImagePreview from "../../../components/image-preview";
+import Skeleton from "../../../components/skeleton";
+import Text from "../../../components/text";
+import type { Photo } from "../models/photo";
+import { Link } from "react-router";
+
+interface PhotoWidgetProps {
+    photo: Photo;
+    loading?: boolean;
+}
+
+export default function PhotoWidget({ photo, loading }: PhotoWidgetProps) {
+    return (
+        <div className="flex flex-col gap-4">
+            {!loading ? (
+                <ImagePreview src={photo.imageId} alt={photo.title} imageClassName="w-[10.875rem] h-[10.875rem] rounded-lg" />
+            ) :
+                <Skeleton rounded="lg" className="w-[10.875rem] h-[10.875rem] rounded-lg" />
+            }
+            <div className="flex flex-col gap-2">
+                {!loading ? 
+                    (
+                        <Text variant="paragraph-large" className="truncate">{photo.title}</Text>
+                    ) 
+                    : 
+                    (
+                        <Skeleton rounded="sm" className="w-full h-6" />
+                    )
+                }
+
+                <div className="flex gap-1 min-h-[1.375rem]">
+                    {!loading ? (
+                        <>
+                            {photo.albums.slice(0, 1).map(album => (
+                                <Badge key={album.id} variant="ghost" size="xs">
+                                    {album.title}
+                                </Badge>
+                            ))}
+                            {photo.albums.length > 1 && (
+                                <Badge variant="ghost" size="xs">
+                                    +{photo.albums.length - 1}
+                                </Badge>
+                            )}
+                        </>
+                    ) 
+                    : 
+                    (
+                        Array.from({ length: 2 }).map((_, index) => (
+                            <Skeleton
+                              key={`album-loading-${index}`}
+                              className="w-full h-4 rounded-sm"
+                            />
+                          ))
+                    )
+                    }
+                </div>
+                {!loading ? (
+                        <Link 
+                            to={`/albums/${photo.albums[0].id}`} 
+                            className={
+                                buttonVariants({ variant: "secondary", size: "sm",  className:"px-2 py-2" })}>
+                            <Text 
+                                variant="label-small"
+                                className={buttonTextVariants({ variant: "secondary", size: "sm" })}>
+                                Detalhe da imagem
+                            </Text>
+                        </Link>
+                    ) : (
+                        <Skeleton rounded="sm" className="w-full h-10" />
+                    )}
+            </div>
+        </div>
+    );
+}
+
+
--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,10 +1,78 @@
 
+import Container from "../components/container";
 import Text from "../components/text";
+import PhotoWidget from "../contexts/photos/components/photo-widget";
 
 export default function PageHome() {
+  
   return (
-    <>
-      <Text variant="heading-medium">Página inicial</Text>
-    </>
+    <Container>
+      <div className="grid grid-cols-5 gap-9">
+        <Text variant="heading-medium">Página inicial</Text>
+        <PhotoWidget photo={
+          {
+            id: "1",
+            title: "Foto 1",
+            imageId: "https://images.pexels.com/photos/16197273/pexels-photo-16197273.jpeg",
+            albums: [
+              {
+                id: "1",
+                title: "Album 1",
+              },
+              {
+                id: "2",
+                title: "Album 2",
+              },
+              {
+                id: "3",
+                title: "Album 3",
+              },
+            ],
+          }
+        } loading={false} />
+        <PhotoWidget photo={
+          {
+            id: "1",
+            title: "Foto 1",
+            imageId: "https://images.pexels.com/photos/16197273/pexels-photo-16197273.jpeg",
+            albums: [
+              {
+                id: "1",
+                title: "Album 1",
+              },
+              {
+                id: "2",
+                title: "Album 2",
+              },
+              {
+                id: "3",
+                title: "Album 3",
+              },
+            ],
+          }
+        } loading={false} />
+        <PhotoWidget photo={
+          {
+            id: "1",
+            title: "Foto 1",
+            imageId: "https://images.pexels.com/photos/16197273/pexels-photo-16197273.jpeg",
+            albums: [
+              {
+                id: "1",
+                title: "Album 1",
+              },
+              {
+                id: "2",
+                title: "Album 2",
+              },
+              {
+                id: "3",
+                title: "Album 3",
+              },
+            ],
+          }
+        } loading={false} />
+      </div>
+    </Container>
   );
 }
