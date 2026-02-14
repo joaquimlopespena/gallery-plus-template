9c88b2553e20f593845009f48a2bf556c750b380
JoaquimLP <joaquimlp2013@gmail.com>
2026-02-14 15:19:36 -0300

    New: add nuqs dependency and integrate NuqsAdapter in App component; refactor AlbumsFilter to utilize usePhotos hook for album filtering

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,30 @@ import LayoutMain from "./pages/layout-main";
 import PageHome from "./pages/page-home";
 import PagePhotoDetails from "./pages/page-photo-details";
 import { useForm } from "react-hook-form";
+import { NuqsAdapter } from 'nuqs/adapters/react-router/v7'
 
 const queryClient = new QueryClient();
 
 
 export default function App() {
-	const form = useForm();
-	const file = form.watch("file");
-	const fileSrc = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
-	return (
-		<QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LayoutMain />}>
-            <Route index element={<PageHome />} />
-            <Route path="/photo/:id" element={<PagePhotoDetails />} />
-            <Route path="/components" element={<PageComponents />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+  const form = useForm();
+  const file = form.watch("file");
+  const fileSrc = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
+  
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NuqsAdapter>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LayoutMain />}>
+              <Route index element={<PageHome />} />
+              <Route path="/photo/:id" element={<PagePhotoDetails />} />
+              <Route path="/components" element={<PageComponents />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </NuqsAdapter>
     </QueryClientProvider>
+
 	);
 }
