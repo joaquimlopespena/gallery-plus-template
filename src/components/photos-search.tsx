71e183bee6c2f09bdc2e666179b41a611a3593c6
JoaquimLP <joaquimlp2013@gmail.com>
2026-02-14 15:23:41 -0300

    Refactor: integrate search functionality in PhotosSearch component using usePhotos hook

--- src/components/photos-search.tsx
+++ src/components/photos-search.tsx
@@ -2,12 +2,17 @@ import InputText from "./input-text";
 import SearchIcon from "../assets/icons/search.svg?react";
 import { useCallback, useState } from "react";
 import { debounce } from "../helpers/utils";
+import usePhotos from "../contexts/photos/hooks/use-photos";
 
 export default function PhotosSearch() {
     const [search, setSearch] = useState("");
+    const { filters } = usePhotos();
+
     const debouncedSetSearch = useCallback(
-        debounce((value: string) => console.log(value), 800),
-        []
+        debounce((value: string) => {
+            filters.setQ(value);
+        }, 800),
+        [filters.setQ]
     );
 
     function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
