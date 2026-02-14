58db95424cbbbfe3ff5c9efe4b148fb924fff569
JoaquimLP <joaquimlp2013@gmail.com>
2026-02-14 15:26:59 -0300

    Refactor: conditionally render PhotosSearch and Divider in MainHeader based on pathname

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -1,7 +1,7 @@
 import type { ComponentProps } from "react";
 import Container from "./container";
 import LogoIcon from "../assets/images/logo.svg?react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import cx from "classnames";
 import Button from "./button";
 import PhotosSearch from "./photos-search";
@@ -9,22 +9,31 @@ import Divider from "./divider";
 import PhotoNewDialog from "../contexts/photos/components/photo-new-dialog";
 import AlbumNewDialog from "../contexts/albums/components/album-new-dialog";
 
-interface MainHeaderProps extends ComponentProps<typeof Container> {}
+interface MainHeaderProps extends ComponentProps<typeof Container> { }
 
-export default function MainHeader({className, ...props}: MainHeaderProps) {
+export default function MainHeader({ className, ...props }: MainHeaderProps) {
+    const { pathname } = useLocation();
+
     return (
         <Container as="header" className={cx("flex items-center justify-between gap-10", className)} {...props}>
             <Link to="/">
                 <LogoIcon className="h-5" />
             </Link>
-            <PhotosSearch />
-            <Divider orientation="vertical" className="h-10" />
+            {
+                pathname === "/" && (
+                    <>
+                        <PhotosSearch />
+                        <Divider orientation="vertical" className="h-10" />
+                    </>
+
+                )
+            }
 
             <div className="flex items-center gap-3">
                 <PhotoNewDialog trigger={<Button>Nova foto</Button>} />
                 <AlbumNewDialog
-          trigger={<Button variant="secondary">Criar álbum</Button>}
-        />
+                    trigger={<Button variant="secondary">Criar álbum</Button>}
+                />
             </div>
         </Container>
     );
